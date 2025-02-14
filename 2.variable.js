33d5b2eafb079392efc9a9dcde555c93f67e2d94
ShakibulDeveloper <epiktheme@gmail.com>
2025-02-15 02:36:14 +0600

    variable

---
+++ 2.variable.js
@@ -0,0 +1,19 @@
+/*
+variable syntax in js.
+
+let variableName = value;
+
+    [
+        var [used in old javascript].
+        let [instead var use let].
+        const [for fixed variable].
+    ]
+
+*/
+
+let firstNumber = 5;
+let secondNumber = 4;
+console.log(firstNumber+secondNumber);
+
+let message = 'Hello World';
+console.log(message);
